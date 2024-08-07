b85acd3516d8a211e2e2f91a5fd363c496f76803
박주원 <qkrwndnjs7308@gmail.com>
2024-08-07 22:46:07 +0900

    jwt 로그인 회원가입

---
+++ src/main/java/com/example/dayt/dayt/domain/auth/api/AuthController.java
@@ -0,0 +1,42 @@
+package com.example.dayt.dayt.domain.auth.api;
+
+import com.example.dayt.dayt.domain.auth.application.LoginService;
+
+import com.example.dayt.dayt.domain.auth.application.RegisterService;
+import com.example.dayt.dayt.domain.auth.application.ReissueService;
+import com.example.dayt.dayt.domain.auth.dto.request.LoginRequest;
+import com.example.dayt.dayt.domain.auth.dto.request.RefreshTokenRequest;
+import com.example.dayt.dayt.domain.auth.dto.request.RegisterRequest;
+import com.example.dayt.dayt.domain.auth.dto.response.LoginResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final LoginService loginService;
+
+    private final RegisterService registerService;
+
+    private final ReissueService reissueService;
+
+
+
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody @Valid LoginRequest request){
+        return loginService.login(request);
+    }
+
+    @PostMapping("/register")
+    public void register(@RequestBody @Valid RegisterRequest request) {
+        registerService.register(request);
+    }
+
+    @PatchMapping("/reissue")
+    public LoginResponse reissue(@RequestBody @Valid RefreshTokenRequest request){
+        return reissueService.reissue(request);
+    }
+}
